77f06c2521df98b805974b42fef16f65b6148a54
sebastien.huot <huotseb@gmail.com>
2015-03-02 17:39:01 +0100

    Add Use MediaUsageTranslation

--- Entity/MediaUsage.php
+++ Entity/MediaUsage.php
@@ -2,6 +2,7 @@
 
 namespace Bigfoot\Bundle\MediaBundle\Entity;
 
+use Bigfoot\Bundle\MediaBundle\Entity\Translation\MediaUsageTranslation;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
